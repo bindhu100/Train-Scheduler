cbe8fc64b7342426ac988273f35538974241b17c
Bindhu <biji_100@yahoo.com>
2019-02-08 12:06:35 -0700

    update changes

--- assets/javascript/train.js
+++ assets/javascript/train.js
@@ -13,11 +13,7 @@ var config = {
   // variable to referacnce the firebase database
   var database = firebase.database();
 
-  // variable for click event
-  // var name;
-  // var destination;
-  // var firstTrain;
-  // var frequency = 0;
+ 
 
   //  button for adding train
   $("#add-train-btn").on("click", function(){
@@ -29,13 +25,15 @@ var config = {
     var destination = $("#destination").val().trim();
     var firstTrain  = $("#first-train").val().trim();
     var frequency = $("#frequency").val().trim();
+   
+  
 
     // Uploads employee data to the database
     database.ref().push({
       name: name,
       destination: destination,
       firstTrain:firstTrain,
-      frequency,frequency,
+      frequency:frequency,
       dateAdded: firebase.database.ServerValue.TIMESTAMP
 
     });
@@ -51,9 +49,30 @@ var config = {
   ClearFields();
   });
 
-  // Create Firebase event for adding traiin to the database and a row in the html when  adds an entry
+  // delete 3333333333333333333333333333333
+  // function add_block() {
+  //   var curDiv = $('#parent');
+  //   var i = $('#parent div').size()/4 + 1;
+  //   var newDiv='<div id="block_'+ i + '" class="parent_div">' +
+  //   '<div>' +
+  //   '<input type="text">' +
+  //   '</div>' +
+  //   '<img src="arrow.jpg">' +
+  //   '<div>' +
+  //   '<input type="text">' +
+  //   '</div><div><a class="remove_block" href="#">Remove</a></div>' +
+  //   '</div>';
+  //   $(newDiv).appendTo(curDiv);
+  //   };
+  //   $('#parent').on('click', 'a.remove_block', function(events){
+  //     $(this).parents('div').eq(1).remove();
+  //  });
+  //  add_block() 
+  // 333333333333333333333333333
+
+  // Create Firebase event for adding train to the database and a row in the html when  adds an entry
   // 9999999999999999999999999999
-  // clock
+  // display time in jumbotron Html-<p id="time"></p>
   var datetime = null,
         date = null;
 
@@ -70,14 +89,33 @@ $(document).ready(function(){
   // 999999999999999999999999
 
   database.ref().on("child_added", function(childSnapshot) {
+    var nextArr;
+    var minAway;
 
+     // First Time (pushed back 1 year to make sure it comes before current time)
+     var firstTimeConverted = moment(childSnapshot.val().firstTrain, "HH:mm").subtract(1, "years");
 
+     // Difference between the times
+     var diffTime = moment().diff(moment(firstTimeConverted), "minutes");
+
+     // Time apart (remainder)
+     var remainder = diffTime %  childSnapshot.val().frequency;
+
+     // Minute Until Train
+    var minAway = childSnapshot.val ().frequency - remainder;
+
+    // Next Train
+    var nextTrain = moment().add(minAway, "minutes");
+    nextTrain = moment(nextTrain).format("HH:MM");
 
 
     // add data to tbody in html "<tbody id="add-newRow"> </tbody>"
     $("#add-newRow").append("<tr><td>" + childSnapshot.val().name+
     "</td><td>" + childSnapshot.val().destination +
-    "</td><td>" + childSnapshot.val().frequency +"</td><td>");
+    "</td><td>" + childSnapshot.val().frequency +
+    "</td><td>"+ nextTrain +
+    "</td><td>" + minAway + "</td></tr>")
+
 
     
      // Handle the errors
@@ -85,6 +123,11 @@ $(document).ready(function(){
       console.log("Errors handled: " + errorObject.code);
      
     });
+
+
+    // 3333333333333333333333333333333
+ 
+    // 3333333333333333333333333333
     
     database.ref().orderByChild("dateAdded").limitToLast(1).on("child_added", function(snapshot) {
       // Change the HTML to reflect
